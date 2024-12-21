faf737ba460194d89766f18c206b2dc32d50e414
Mohd Zaid <92142397+zaidk22@users.noreply.github.com>
2024-12-21 13:11:00 +0530

    Add files via upload

--- python/day2/task.py
+++ python/day2/task.py
@@ -11,18 +11,94 @@
 
 #integer = whole number
 
-print(123+345)
+# print(123+345)
 
-#Large integer 
+# #Large integer 
 
-print(737378383181398389)
+# print(737378383181398389)
 
-print(737_3783_83181398389) # same for visualization
+# print(737_3783_83181398389) # same for visualization
 
 
 # floats = Floating point number
 
-print(3.1428)
+# print(3.1428)
 
-# boolean
-print(True)
+# # boolean
+# print(True)
+
+
+# # check data type
+
+
+# print(type("Hekkooo"))
+
+
+# type conversion or type casting
+
+# a=  int("123")+int("2")
+# print(a)
+
+# int(), float(), str() , bool()
+
+
+#mathematical oprations in python
+
+# print(7-3)
+# print(7+3)
+# print(7*3)
+# print(7/3)  #float
+# print(7 // 3) #remove decimal points give int
+
+# print(2**3)  #exponent 2^3
+
+# prority operation
+
+#PEMDAS
+# left to right
+# ()
+# **
+# * OR /
+# + OR -
+
+# print(3*3+3/3-3)
+
+# # to get 3 change code
+
+# print(3*(3+3)/3-3)
+
+# bmi = 84/1.65**2
+
+# print(bmi)
+
+# #flooring
+
+# print(int(bmi))
+
+# # rounding
+
+# print(round(bmi))
+
+
+# print(round(bmi,2))
+
+
+# f string
+
+# score = 0
+
+# print(f"your score is {score}")
+
+
+# tip calculator
+
+
+# print("Welcome to the tip calculator!")
+# totalBill = float(input("What was the total bill?"))
+# tipRate = float(input("How much tip would you like to give? 10, 12, or 15?"))
+# peopleCount = int(input("How many people to split the bill?"))
+
+# amount = (totalBill+ totalBill*tipRate/100)/peopleCount
+
+# print(f"Each person should pay: {round(amount,)}")
+
