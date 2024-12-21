56cef0288d50e02381e612773f73e022e1aa4045
Mohd Zaid <92142397+zaidk22@users.noreply.github.com>
2024-12-21 11:52:50 +0530

    Add files via upload

---
+++ python/day1/main.py
@@ -0,0 +1,24 @@
+
+# print("'hello world!'")
+
+# print("'hello world!\nHello new line'")
+
+
+# concatenation
+
+# print("hello"+" "+"zaid")
+
+
+#  input function
+
+#input(a prompt to user)
+
+
+# print("hello " + input('what is your name?'))
+
+# print("hello " + input('what is your name?')+"!")
+
+
+
+
+
