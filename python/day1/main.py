ef5c7ee09e2d696656cdb589606011c042d7e2b3
Mohd Zaid <92142397+zaidk22@users.noreply.github.com>
2024-12-21 12:27:45 +0530

    Add files via upload

--- python/day1/main.py
+++ python/day1/main.py
@@ -19,6 +19,30 @@
 # print("hello " + input('what is your name?')+"!")
 
 
+# variable
+# name = "Jack"
+# print(name)
+
+# name = "zaid"
+# print(name)
+
+
+#length of string
+
+
+
+# name = "zaid"
+# print(len(name))
+
+# print(len(input("What is your name ?")))
+
+
+# band name generator
+# print("welcome to band name generator")
+
+# city = input("Which city yo grew up in ?\n")
+# pet =input("what is the name of your pet?\n")
+# print("your band name could be :"+ city +" "+ pet)
 
 
 
---
+++ python/day2/task.py
@@ -0,0 +1,28 @@
+# data types in python
+
+#String
+
+#subscripting
+# print("hello"[1])
+
+# # count from end
+# print("hello"[-1])
+
+
+#integer = whole number
+
+print(123+345)
+
+#Large integer 
+
+print(737378383181398389)
+
+print(737_3783_83181398389) # same for visualization
+
+
+# floats = Floating point number
+
+print(3.1428)
+
+# boolean
+print(True)
